207c6d4de5d5670ebb19410dde840018b053e11b
LukeKeam <luke_keam@hotmail.com>
2022-11-01 12:17:56 +1000

    fix

--- run_python.bat
+++ run_python.bat
@@ -1,4 +1,4 @@
 SET ThisScriptsDirectory=%~dp0
 SET PyScriptPath="%ThisScriptsDirectory%main.py"
-%PyScriptPath%\venv\Scripts\python.exe "%PowerShellScriptPath%"
-
+%ThisScriptsDirectory%venv\Scripts\python.exe %PyScriptPath%
+pause

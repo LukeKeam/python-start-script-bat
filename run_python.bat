c20776767d09256b3c60d1979e6c130d43e75556
LukeKeam <luke_keam@hotmail.com>
2022-12-24 10:35:52 +1000

    fix quotes

--- run_python.bat
+++ run_python.bat
@@ -1,4 +1,4 @@
 SET ThisScriptsDirectory=%~dp0
 SET PyScriptPath="%ThisScriptsDirectory%main.py"
-%ThisScriptsDirectory%venv\Scripts\python.exe %PyScriptPath%
+"%ThisScriptsDirectory%venv\Scripts\python.exe" %PyScriptPath%
 pause
